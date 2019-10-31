fadf5ea1c998c49bea6227b91c2c98ea7b315600
tuh12085 <tuh12085@temple.edu>
2019-10-31 10:14:27 -0400

    update

--- project 3/run.c
+++ project 3/run.c
@@ -21,8 +21,7 @@ pthread_cond_t consumer = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 
-
-int limit = 10;
+int limit = 2;
 int size_counter = 0;
 
 
@@ -39,19 +38,21 @@ int main(){
     printf("hello from the sleep.c\n");
     pthread_t thread = NULL;
     
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 20; i++) {
         struct item *thread_1 = (struct item *)malloc(sizeof(struct item));
         thread_1->threadID = i;
         pthread_create(&thread,NULL,function,(void*)thread_1);
     }
     
     pthread_join(thread, NULL);
+    
     printf("\n\n\n\n");
     return 0;
 }
 
 void* function(void * input){
-            
+    
+    
     int number = ((struct item*)input)->threadID;
     printf("age: %d\n",number);
 
@@ -61,38 +62,38 @@ void* function(void * input){
                     
     
     if(size_counter == limit){
-        printf("------zzzzzz------\n");
+        printf("Thread %d go to sleep  --------------------- \n",number);
         pthread_cond_wait(&producer, &lock);
         
     }else{
         size_counter++;
     }
     
-    pthread_cond_signal(&consumer);
+    pthread_cond_broadcast(&consumer);
     pthread_mutex_unlock(&lock);
     
     
     //consumer
     pthread_mutex_lock(&lock);
-    printf("Thread ID is [%d]",pthread_self());
+    
     
     if(size_counter == 0){
-        printf("------zzzzzz------\n");
+        printf("Thread %d go to sleep  --------------------- \n",number);
         pthread_cond_wait(&consumer, &lock);
         
     }else{
         if(number%2 == 0){
-            printf("Number %d This is an even\n",number);
+            printf("Thread %d This is an even\n",number);
         }else{
-            printf("Number %d This is not an even\n",number);
+            printf("Thread %d This is an odd\n",number);
         }
         
         size_counter--;
     }
-    pthread_cond_signal(&producer);
     
-    
+    pthread_cond_broadcast(&producer);
     pthread_mutex_unlock(&lock);
+    
     
     return 0;
 }
