cba2ca57d1971d850569e4fd6ab6d8bca3dcdb8d
tuh12085 <tuh12085@temple.edu>
2019-11-14 11:35:38 -0500

    up

---
+++ project 4/4B/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#include "disk.h"
+
+int main(){
+    printf("hello world !\n");
+    return 0;
+}
