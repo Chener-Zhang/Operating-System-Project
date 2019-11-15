70efcf732c2d0a133cf4a3c43f715c5e490f3fe4
tuh12085 <tuh12085@temple.edu>
2019-11-14 21:41:26 -0500

    delete_block() Finished

--- project 4/4B/mydisk.c
+++ project 4/4B/mydisk.c
@@ -9,17 +9,13 @@ int each_block_size = 16;
 int fd;
 // --------------------------------------------- Create a disk ---------------------------------------------//
 int create_disk(char *name ){
-
-
-
-
     int file_size_counter;
     fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0644);  
 
     char buf[each_block_size];
     memset(buf, 0, each_block_size);
 
-        for (file_size_counter = 0;  file_size_counter < number_of_block; file_size_counter++){
+    for (file_size_counter = 0;  file_size_counter < number_of_block; file_size_counter++){
                 write(fd, buf, each_block_size);
         }
 
@@ -45,11 +41,22 @@ int write_disk(int block_index, char* words){
 // --------------------------------------------- Read a disk/block ---------------------------------------------//
 
 int read_disk(int block_index){
-    lseek(fd, 0, SEEK_SET);    
     lseek(fd, block_index * each_block_size, SEEK_SET);    
     char read_buffer[each_block_size];
     read(fd,read_buffer,strlen(read_buffer));
     printf("%s",read_buffer);
+    return 0;
+}
+// --------------------------------------------- Delete a disk/block ---------------------------------------------//
+
+int detele_block(int block_index){
+    lseek(fd, block_index * each_block_size, SEEK_SET);  
+
+    char buf[each_block_size];
+    memset(buf, 0, each_block_size);
+    write(fd, buf, each_block_size);
+    printf("\n this is the buf [%s]\n",buf);
+    
     return 0;
 }
 // --------------------------------------------- Close a disk ---------------------------------------------//
@@ -69,8 +76,7 @@ int main(){
     write_disk(0,"one");
     write_disk(1,"two");
     write_disk(2,"three");
-    write_disk(4,"three");
-
+    detele_block(0);
 
     close_disk(fd);
 
