fc6553e2765974c39f8424fa4d26e8de6a93d46e
tuh12085 <tuh12085@temple.edu>
2019-11-17 17:14:34 -0500

    Update struct.h

--- project 4/4B/struct.h
+++ project 4/4B/struct.h
@@ -21,13 +21,7 @@ struct File {
 struct Direction{    
     char name[name_size];// name of the direction    
     int current_index;
-    int previous_index;
-
-    struct File* file_list[file_list_number]; // file list here
-    int file_list_tracking[file_list_number]; 
-
-    struct Direction* next_entry_direction[direction_list_number]; // directory list here
-    int next_entry_direction_tracking[direction_list_number];
+    int previous_index;        
     // add additional imformation here
 };
 
