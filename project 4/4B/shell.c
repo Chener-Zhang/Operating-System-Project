c9f6e3ee3ea64344249cdf732274f423c9e13168
tuh12085 <tuh12085@temple.edu>
2019-11-18 10:35:11 -0500

    Update shell.c

--- project 4/4B/shell.c
+++ project 4/4B/shell.c
@@ -11,6 +11,14 @@
 
 int main(int argc, char **argv)
 {
+    char cmd[20] = "hello";
+    printf("%s\n",cmd);
+    return 0;
+    
+}
+
+
+int parsing(){
     char cmd[20];
     char arg[20];
     char copy[20];
@@ -19,9 +27,8 @@ int main(int argc, char **argv)
     fgets(user_input,20,stdin); // get user_input
     strcpy(copy,user_input); // copy just in case    
     strcpy(cmd,strtok(user_input," ")); // assign first arg -> cmd;
-    printf("[%s]", cmd);
-    
+    printf("[%s]\n", cmd);
+    strcpy(arg,strtok(NULL," \n"));
+    printf("[%s]\n",arg);
     return 0;
-
-    
 }
