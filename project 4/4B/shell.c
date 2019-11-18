58e47a67c25d824511f55fbbc8195a3eab54bc07
tuh12085 <tuh12085@temple.edu>
2019-11-17 23:14:16 -0500

    shell

---
+++ project 4/4B/shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <strings.h>
+#include "struct.h"
+
+int main(){
+    printf("hello world\n");
+    
+    return 0;
+}
