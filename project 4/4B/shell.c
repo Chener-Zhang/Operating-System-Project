52d06e8b31c112a6c31cf8e726babdba73df80ae
tuh12085 <tuh12085@temple.edu>
2019-11-18 10:22:50 -0500

    Update shell.c

--- project 4/4B/shell.c
+++ project 4/4B/shell.c
@@ -4,25 +4,15 @@
 #include <fcntl.h>
 #include <string.h>
 #include <strings.h>
+#include <errno.h>
+
 #include "struct.h"
 
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "usage: %s file\n", argv[0]);
-        return 1;
-    }
 
-    FILE *fp = fopen(argv[1], "rb");
-    if (!fp) {
-        // alternatively, just `perror(argv[1])`
-        fprintf(stderr, "cannot open %s: %s\n", argv[1], strerror(errno));
-        return 1;
-    }
 
-    // read from fp here
 
-    fclose(fp);
     return 0;
 }
