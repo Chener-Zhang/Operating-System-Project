5fb5485c2fc421b644c7480c9bf6cd2799437299
tuh12085 <tuh12085@temple.edu>
2019-11-17 23:21:47 -0500

    Update shell.c

--- project 4/4B/shell.c
+++ project 4/4B/shell.c
@@ -6,8 +6,29 @@
 #include <strings.h>
 #include "struct.h"
 
+
+
+int my_shell(){
+    char user_input[20];
+    gets(user_input);
+    printf(">\n");
+    printf("you have enter \n [%s]\n",user_input);
+
+    if(strcmp(user_input,"quit")== 0){        
+            exit(0);
+    }
+
+    return 0;
+}
+
 int main(){
-    printf("hello world\n");
+
+
+    while(1){
+
+        my_shell();
+
     
+    }
     return 0;
 }
