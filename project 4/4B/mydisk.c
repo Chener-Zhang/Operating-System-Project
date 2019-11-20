0d746ab5ac9e0c949ae0a4b9b008626338237782
tuh12085 <tuh12085@temple.edu>
2019-11-20 14:10:23 -0500

    Update mydisk.c

--- project 4/4B/mydisk.c
+++ project 4/4B/mydisk.c
@@ -580,13 +580,19 @@ int Delete_file(char *filename, struct Direction *dir_table[], struct Direction 
                     {
                         if(current == i)
                         {
-                            printf("%d - ",i);                            
+                                                        
+                            printf("\n");
                             current = blocktable[i]->next_block;
+                            printf("current %d\n",current);                            
                             blocktable[previous]->next_block = 0;
+                            printf("previous %d\n",previous);
+                            blocktable[previous]->used = 0;
                             previous = current;
-                            
                             //update tracker
+                            printf("previous %d\n",previous);
                         }
+                                                                               
+
                     }
                     
                     //delete the block
