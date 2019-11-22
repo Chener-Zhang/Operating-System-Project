3666da6f256a94cd96afe4bada719f1eb13caa14
tuh12085 <tuh12085@temple.edu>
2019-11-22 12:02:38 -0500

    update

--- project 4/4B/struct.h
+++ project 4/4B/struct.h
@@ -3,12 +3,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+
+
+
 #define name_size  10   //define
 #define bloc_entry_number  30   // for calculation
 #define direction_list 30  // [#] plugin number 
 // DIY the var here:
 #define file_list 30     // [#] plugin number 
 #define block_list 30   // [#] plugin number 
+
+
+
 // Note from myself: Assignment requirement;  
 // 16,384 blocks total     ---- 2^14
 // 8,192 for data blocks   ----- 2^13
@@ -24,6 +30,7 @@ data - > 32 ---> 2^5;
 struct File;
 struct Direction;
 struct Block;
+
 struct File { 
     char name[name_size]; //index of the block which contains the imformation of the file;
     int below_direction;
@@ -31,9 +38,9 @@ struct File {
     int used;
     int first_block_entry;
     int meta_block_entry;
-
-    //done
-    // int next_block_entry;    
+    int last_pointer;
+    int last_pointer_remainder;
+    int last_block_id;
 };
 
 struct Direction{    
@@ -45,6 +52,7 @@ struct Direction{
     int meta_block_entry;
     // add additional imformation here
 };
+
 struct Block{
     int next_block; // the id / the index of the block
     int size_remain; // the size of a single block remain 
@@ -54,3 +62,37 @@ struct Block{
 // ---------------- Function declares --------------------------- 
 int char_reset();
 int parsing();
+
+
+int mount(char *name);
+int unmount(int fd);
+
+int get_free_space_dirtable(struct Direction *list[]);
+int get_free_space_filetable(struct File *list[]);
+int init_dir(struct Direction *list[]);
+int init_root(struct Direction *list[]);
+
+int create_disk(char *name);
+int open_disk(char *name);
+int write_disk(int block_index, char* words);
+int read_disk(int block_index);
+int delete_block(int block_index);
+int close_disk(int fd);
+
+int init_block_allocation();
+int init_block(struct Block *lisk[]);
+
+int init_file(struct File *list[]);
+int Create_file(char *filename, struct Direction *current_dir,struct Direction *dir_table[],struct File *file_table[],struct Block *meta_block_table[]);
+int Write_file(char *filename, struct Direction *current_dir,struct Direction *dir_table[],struct File *file_table[],struct Block *block_table[]);
+int Read_file(char *filename, struct Direction *dir_table[], struct Direction *current_dir,struct File *file_table[]);
+int Delete_file(char *filename, struct Direction *dir_table[], struct Direction *current_dir,struct File *file_table[]);
+
+
+int Create_directory(char *dirname, struct Direction *dir_table[], struct Direction *current_dir);
+int Change_directory(char *dirname,struct Direction *current_dir, struct Direction *dir_table[]);
+int Delete_directory(char *dirname, struct Direction *dir_table[], struct Direction *current_dir,struct File *file_table[]);
+void print_direction( struct Direction *dir,struct Direction *list[],struct File *file_table[]);
+int print_list();
+int begin();
+// ---------------- Function declares ---------------------------
